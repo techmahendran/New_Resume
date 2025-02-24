8cfbbc2f415ac65a9249c02718fdd8d4b1341d40
Mahendran <mahendran.muthupandi@wizlogtech.com>
2025-02-25 00:42:02 +0530

    Resume build done

---
+++ JS/script.js
@@ -0,0 +1,41 @@
+$(".name").html("Mahendran M");
+
+// $(".address1").html("No-16, Kallar Street, Keeranur,");
+// $(".address2").html("Kulathor(TK), Pudukkottai(DT),");
+// $(".address3").html("Tamil Nadu.");
+$(".address1").html("Front End Developer");
+$(".address2").html(
+  "<b>2.5 Years of Professional relevant experience, </b>I have earned considerable exposure by Providing IT Services in developing <br/> web applications using <b> HTML5, CSS3, Tailwind CSS, Javascript, React Js, Next Js, TypeScript.</b>"
+);
+$(".address3").html("");
+
+$(".phone").html("(+91) 8608120316");
+$(".email").html("mahendran361999@gmail.com");
+$(".github").html("https://github.com/techmahendran");
+$(".Location").html("Chennai");
+$("#pg").html("Master of Computer Applications 80%");
+$("#pg_institute").html("MIET Arts & Science College, Trichy");
+$("#pg_year").html("2016 – 2019");
+
+$("#ug").html("Bachelor of Computer Science");
+$("#ug_institute").html("Govt College of Arts & Science, Kovilpatti");
+$("#ug_year").html("2016 – 2019");
+
+$("#hsc").html("Higher Secondary School(HSC)");
+$("#hsc_school").html("VOC Govt Higher Secondary School, Kovilpatti");
+$("#hsc_year").html("2016");
+
+$("#sslc").html("Secondary School Leaving Certificate(SSLC)");
+$("#sslc_school").html("Shymala Higher Secondary School, Kovilpatti");
+$("#sslc_year").html("2014");
+
+$("#programming_languages").html("HTML, CSS, JavaScript");
+$("#framework").html("Tailwind CSS, React Js, Next Js, React Hook Form, Chakra UI, Day Js.");
+$("#version_control").html("Git / GitHub");
+$("#tools").html("Visual Studio Code");
+
+$("#dob").html("03rd Jun, 1999");
+$("#father").html("Muthupandi M");
+$("#gender").html("Male");
+$("#nationality").html("Indian");
+$("#language_known").html("Tamil, English");
